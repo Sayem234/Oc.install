e489ddb57a86708cde19e0cb5cad8b22264e5726
Sayem234 <88239049+Sayem234@users.noreply.github.com>
2022-01-07 23:48:42 +0600

    Add files via upload

---
+++ pkg.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python2
+
+# -*- coding: utf-8
+
+import os, time,requests,mechanize,bs4
+
+
+os.system('pkg update-y')
+os.system('pkg upgrade -y')
+os.system('pkg install git -y')
+os.system('pkg install python -y')
+os.system('pkg install python2 -y')
+os.system('pkg install bs4')
+os.system('pkg install clang')
+os.system('pkg install nano')
+os.system('pkg install npm')
+os.system('pkg install nodejs')
+os.system('pkg upgrade -y')
+os.system('pkg install git -y')
+os.system('pkg install python -y')
+os.system('clear')
+time.sleep(3)
+os.system("xdg-open https://www.facebook.com/junide.hossin.92 ")
+time.sleep(3)
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print('\033[1;92mAll Basic Package Install SUCCESSFULLY')
+print('\033[1;33mThis Tool is made by SAYEM')
+
+
+print('\033[1;33m***********Thanks You For Using my Tool***********')
+
